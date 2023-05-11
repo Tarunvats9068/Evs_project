fc3333f41d17cf1609a9854ff566849fb652b53a
Tarun Sharma <82764100+Tarunvats9068@users.noreply.github.com>
2023-05-11 09:34:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express  = require('express');
 const app = express();
-const port = 9000;
+const port = process.env.PORT || 9000;
 const db = require('./config/mongoose');
 const passport = require('passport');
 const session = require('express-session');
@@ -28,7 +28,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 app.use('/',require('./routes'));
-app.listen(port,'10.0.0.128',function(err){
+app.listen(port,function(err){
     if(err){console.log("error in running the server"); return;};
     return console.log(`server is running on url http://10.0.0.128:${port}/`) ;
 
